4360c9d3c409ff2b8626fc228dfc40dc6396c4f1
Camila Puentes <Kamilapava02@hotmail.com>
2023-10-11 18:01:09 -0500

    Simulador de calculadora

---
+++ test/calculadora1/CalculatorIT.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kamil
+ */
+public class CalculatorIT {
+    
+    public CalculatorIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class Calculator.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        int a = 2;
+        int b = 3;
+        Calculator instance = new Calculator();
+        int expResult = 5; // suma 3+2 = 5
+        int result = instance.add(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of subtract method, of class Calculator.
+     */
+    @Test
+    public void testSubtract() {
+        System.out.println("subtract");
+        int a = 10;
+        int b = 5;
+        Calculator instance = new Calculator();
+        int expResult = 5; //resta 10-5 = 5
+        int result = instance.subtract(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of multiply method, of class Calculator.
+     */
+    @Test
+    public void testMultiply() {
+        System.out.println("multiply");
+        int a = 5;
+        int b = 5;
+        Calculator instance = new Calculator();
+        int expResult = 25; // multiplica 5*5 = 25
+        int result = instance.multiply(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of divide method, of class Calculator.
+     */
+    @Test
+    public void testDivide() {
+        System.out.println("divide");
+        int a = 25;
+        int b = 5;
+        Calculator instance = new Calculator();
+        int expResult = 5;//divide 25/5 =5
+        int result = instance.divide(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+    
+}
